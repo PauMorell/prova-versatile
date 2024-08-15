6c37a4d82ececd4e1409c5172cc6fefbf119ebf9
Pablos <paumorell3@gmail.com>
2024-08-15 18:46:27 +0200

    Markers i track funcionals

--- prova-versatile/src/App.jsx
+++ prova-versatile/src/App.jsx
@@ -1,13 +1,19 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import React, { useState, useEffect } from "react";
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from "react-leaflet";
 import "./index.css";
 
 function App() {
-  const darkMap = "mapbox://styles/pableroni/clzvecqzh000301pe8afe73ht";
-  const MapboxToken =
-    "pk.eyJ1IjoicGFibGVyb25pIiwiYSI6ImNsenZlYWJ4dDA3Y3kya3NkejU0M2ZxcmsifQ._wYVoRilNCKpdBqp0VUv_w";
+  const [geoJsonData, setGeoJsonData] = useState(null);
 
-  const APImapBox =
-    "https://api.mapbox.com/v4/pableroni/clzvecqzh000301pe8afe73ht/{x}/{y}{@2x}.{format}";
+  useEffect(() => {
+    // Cargar el archivo GeoJSON
+    fetch("/mapes/map.geojson")
+      .then((response) => response.json())
+      .then((data) => {
+        setGeoJsonData(data);
+      })
+      .catch((error) => console.error("Error cargando el GeoJSON:", error));
+  }, []);
 
   const markers = [
     {
@@ -24,17 +30,28 @@ function App() {
     },
   ];
 
+  const geoJsonStyle = {
+    color: "red",
+    weight: 3,
+    opacity: 0.7,
+  };
+
+  const onEachFeature = (features, layer) => {
+    if (features.properties && features.properties.name) {
+      const popupContent = `
+      <div>
+      <h3>${features.properties.name}</h3>
+      <p>Distància: ${features.properties.distance}</p>
+      <p>Desnivell: ${features.properties.elevation}</p>
+      </div>
+      `;
+      layer.bindPopup(popupContent);
+    }
+  };
+
   return (
     <>
-      <MapContainer center={[39.6953, 3.0176]} zoom={10}>
-        {/* <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        /> */}
-        {/* <TileLayer
-          url="https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicGFibGVyb25pIiwiYSI6ImNsenZlYWJ4dDA3Y3kya3NkejU0M2ZxcmsifQ._wYVoRilNCKpdBqp0VUv_w"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
-        /> */}
+      <MapContainer center={[39.764552, 2.727484]} zoom={14}>
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/pableroni/clzvecqzh000301pe8afe73ht/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicGFibGVyb25pIiwiYSI6ImNsenZlYWJ4dDA3Y3kya3NkejU0M2ZxcmsifQ._wYVoRilNCKpdBqp0VUv_w`}
           attribution='Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
@@ -44,6 +61,13 @@ function App() {
             <Popup className="custom-popup">{marker.popup}</Popup>
           </Marker>
         ))}
+        {geoJsonData && (
+          <GeoJSON
+            data={geoJsonData}
+            style={geoJsonStyle}
+            onEachFeature={onEachFeature}
+          />
+        )}
       </MapContainer>
     </>
   );
