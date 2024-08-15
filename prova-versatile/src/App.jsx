8658712094cf1dd58553ed80c5f1e29eaa034228
Pablos <paumorell3@gmail.com>
2024-08-15 17:31:12 +0200

    Proves funcionals

--- prova-versatile/src/App.jsx
+++ prova-versatile/src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const darkMap = "mapbox://styles/pableroni/clzvecqzh000301pe8afe73ht";
+  const MapboxToken =
+    "pk.eyJ1IjoicGFibGVyb25pIiwiYSI6ImNsenZlYWJ4dDA3Y3kya3NkejU0M2ZxcmsifQ._wYVoRilNCKpdBqp0VUv_w";
+
+  const APImapBox =
+    "https://api.mapbox.com/v4/pableroni/clzvecqzh000301pe8afe73ht/{x}/{y}{@2x}.{format}";
+
+  const markers = [
+    {
+      geocode: [39.758808, 2.747396],
+      popup: "Refugi Cornadors",
+    },
+    {
+      geocode: [39.764552, 2.727484],
+      popup: "Sa Grillona",
+    },
+    {
+      geocode: [39.770013, 2.71552],
+      popup: "Andreu Coll 10",
+    },
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <MapContainer center={[39.6953, 3.0176]} zoom={10}>
+        {/* <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        /> */}
+        {/* <TileLayer
+          url="https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicGFibGVyb25pIiwiYSI6ImNsenZlYWJ4dDA3Y3kya3NkejU0M2ZxcmsifQ._wYVoRilNCKpdBqp0VUv_w"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
+        /> */}
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/pableroni/clzvecqzh000301pe8afe73ht/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicGFibGVyb25pIiwiYSI6ImNsenZlYWJ4dDA3Y3kya3NkejU0M2ZxcmsifQ._wYVoRilNCKpdBqp0VUv_w`}
+          attribution='Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+        />
+        {markers.map((marker) => (
+          <Marker position={marker.geocode} key={marker.popup}>
+            <Popup className="custom-popup">{marker.popup}</Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
